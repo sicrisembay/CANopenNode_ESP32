4249296242adfedc9fa1e8d7eba5e432c76a8d38
sicris <sicris.embay@gmail.com>
2023-11-26 11:14:44 +0800

    + updated build test CMakeLists to include CANopenNode files.

---
+++ example/build_test/README.md
@@ -0,0 +1,5 @@
+# Build Test
+
+This is an oversimplified test project where its main objective is to test the project cmake build with CANopenNode files.
+
+`idf.py build`
--- port/CO_driver_target.h
+++ port/CO_driver_target.h
@@ -22,7 +22,6 @@
  * limitations under the License.
  */
 
-
 #ifndef CO_DRIVER_TARGET_H
 #define CO_DRIVER_TARGET_H
 
@@ -39,75 +38,83 @@
 #endif
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Stack configuration override default values.
  * For more information see file CO_config.h. */
-
 
 /* Basic definitions. If big endian, CO_SWAP_xx macros must swap bytes. */
 #define CO_LITTLE_ENDIAN
 #define CO_SWAP_16(x) x
 #define CO_SWAP_32(x) x
 #define CO_SWAP_64(x) x
-/* NULL is defined in stddef.h */
-/* true and false are defined in stdbool.h */
-/* int8_t to uint64_t are defined in stdint.h */
-typedef uint_fast8_t            bool_t;
-typedef float                   float32_t;
-typedef double                  float64_t;
-
+    /* NULL is defined in stddef.h */
+    /* true and false are defined in stdbool.h */
+    /* int8_t to uint64_t are defined in stdint.h */
+    typedef uint_fast8_t bool_t;
+    typedef float float32_t;
+    typedef double float64_t;
 
 /* Access to received CAN message */
-#define CO_CANrxMsg_readIdent(msg) ((uint16_t)0)
-#define CO_CANrxMsg_readDLC(msg)   ((uint8_t)0)
-#define CO_CANrxMsg_readData(msg)  ((uint8_t *)NULL)
+#define CO_CANrxMsg_readIdent(msg) ((uint16_t)(((CO_CANrxMsg_t *)msg)->ident))
+#define CO_CANrxMsg_readDLC(msg) ((uint8_t)(((CO_CANrxMsg_t *)msg)->DLC))
+#define CO_CANrxMsg_readData(msg) ((uint8_t *)&(((CO_CANrxMsg_t *)msg)->data[0]))
 
-/* Received message object */
-typedef struct {
-    uint16_t ident;
-    uint16_t mask;
-    void *object;
-    void (*CANrx_callback)(void *object, void *message);
-} CO_CANrx_t;
+    /* Received message object */
+    typedef struct
+    {
+        uint16_t ident;
+        uint16_t mask;
+        void *object;
+        void (*CANrx_callback)(void *object, void *message);
+    } CO_CANrx_t;
 
-/* Transmit message object */
-typedef struct {
-    uint32_t ident;
-    uint8_t DLC;
-    uint8_t data[8];
-    volatile bool_t bufferFull;
-    volatile bool_t syncFlag;
-} CO_CANtx_t;
+    typedef struct
+    {
+        uint32_t ident;
+        uint8_t DLC;
+        uint8_t data[8];
+    } CO_CANrxMsg_t;
 
-/* CAN module object */
-typedef struct {
-    void *CANptr;
-    CO_CANrx_t *rxArray;
-    uint16_t rxSize;
-    CO_CANtx_t *txArray;
-    uint16_t txSize;
-    uint16_t CANerrorStatus;
-    volatile bool_t CANnormal;
-    volatile bool_t useCANrxFilters;
-    volatile bool_t bufferInhibitFlag;
-    volatile bool_t firstCANtxMessage;
-    volatile uint16_t CANtxCount;
-    uint32_t errOld;
-} CO_CANmodule_t;
+    /* Transmit message object */
+    typedef struct
+    {
+        uint32_t ident;
+        uint8_t DLC;
+        uint8_t data[8];
+        volatile bool_t bufferFull;
+        volatile bool_t syncFlag;
+    } CO_CANtx_t;
 
+    /* CAN module object */
+    typedef struct
+    {
+        void *CANptr;
+        CO_CANrx_t *rxArray;
+        uint16_t rxSize;
+        CO_CANtx_t *txArray;
+        uint16_t txSize;
+        uint16_t CANerrorStatus;
+        volatile bool_t CANnormal;
+        volatile bool_t useCANrxFilters;
+        volatile bool_t bufferInhibitFlag;
+        volatile bool_t firstCANtxMessage;
+        volatile uint16_t CANtxCount;
+        uint32_t errOld;
+    } CO_CANmodule_t;
 
-/* Data storage object for one entry */
-typedef struct {
-    void *addr;
-    size_t len;
-    uint8_t subIndexOD;
-    uint8_t attr;
-    /* Additional variables (target specific) */
-    void *addrNV;
-} CO_storage_entry_t;
-
+    /* Data storage object for one entry */
+    typedef struct
+    {
+        void *addr;
+        size_t len;
+        uint8_t subIndexOD;
+        uint8_t attr;
+        /* Additional variables (target specific) */
+        void *addrNV;
+    } CO_storage_entry_t;
 
 /* (un)lock critical section in CO_CANsend() */
 #define CO_LOCK_CAN_SEND(CAN_MODULE)
@@ -124,9 +131,16 @@ typedef struct {
 /* Synchronization between CAN receive and message processing threads. */
 #define CO_MemoryBarrier()
 #define CO_FLAG_READ(rxNew) ((rxNew) != NULL)
-#define CO_FLAG_SET(rxNew) {CO_MemoryBarrier(); rxNew = (void*)1L;}
-#define CO_FLAG_CLEAR(rxNew) {CO_MemoryBarrier(); rxNew = NULL;}
-
+#define CO_FLAG_SET(rxNew)  \
+    {                       \
+        CO_MemoryBarrier(); \
+        rxNew = (void *)1L; \
+    }
+#define CO_FLAG_CLEAR(rxNew) \
+    {                        \
+        CO_MemoryBarrier();  \
+        rxNew = NULL;        \
+    }
 
 #ifdef __cplusplus
 }
